b5e29a14d72003da48456d8a12f0f0357f544647
王 <1585049064@qq.com>
2020-02-12 20:28:09 +0800

    新增Hello类

---
+++ src/com/atguigu/bean/Hello.java
@@ -0,0 +1,13 @@
+package com.atguigu.bean;
+
+/**
+ * @author Administrator
+ * @create 2020-02-12 下午 20:22
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("2222");
+    }
+
+}
